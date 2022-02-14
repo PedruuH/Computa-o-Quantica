ec7ae9fbc1038685d8cd919cf774b18559c4a2c0
Pedruuh <pedro.santos@ufu.br>
2022-02-14 13:57:31 -0300

    matplotlib

--- qubit.py
+++ qubit.py
@@ -39,6 +39,59 @@ class Qubit:
         a = colvec[0,0]
         b = colvec[1,0]
         self.set_pa(a,b)
+    @staticmethod
+    def plot(*args, **kwargs):
+        def repel_from_center(x, y, z, m=0.1):
+            return x + (-m if x < 0 else m), \
+                    y + (-m if y < 0 else m), \
+                    z + (-m if z < 0 else m)
+        def bloch_sphere():
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            u = np.linspace(0, 2*np.pi, 30)
+            v = np.linspace(0, np.pi, 20)
+            x = 1 * np.outer(np.cos(u), np.sin(v))
+            y = 1 * np.outer(np.sin(u), np.sin(v))
+            z = 1 * np.outer(np.ones(np.size(u)), np.cos(v))
+            ax.plot_wireframe(x, y, z, color='gray', linestyle=':')
+            ax.plot3D([-1, 1], [0, 0], [0, 0], color='k', linestyle='--')
+            ax.text(-1.1, 0, 0, '$|-\\rangle$', 'x', horizontalalignment='right', \
+                fontweight='bold', fontsize=11)
+            ax.text(1.1, 0, 0, '$|+\\rangle$', 'x', horizontalalignment='left', \
+                fontweight='bold', fontsize=11)
+            ax.plot3D([0, 0], [-1, 1], [0, 0], color='k', linestyle='--')
+            ax.text(0, -1.1, 0, '$|-i\\rangle$', 'y', horizontalalignment='right', \
+                fontweight='bold', fontsize=11)
+            ax.text(0, 1.1, 0, '$|i\\rangle$', 'y', horizontalalignment='left', \
+                fontweight='bold', fontsize=11)
+            ax.plot3D([0, 0], [0, 0], [-1, 1], color='k', linestyle='--')
+            ax.text(0, 0, -1.1, '$|1\\rangle$', 'x', horizontalalignment='center', \
+                fontweight='bold', fontsize=11)
+            ax.text(0, 0, 1.1, '$|0\\rangle$', 'x', horizontalalignment='center', \
+                fontweight='bold', fontsize=11)
+            limits = np.array([getattr(ax, f'get_{axis}lim')() \
+                for axis in 'xyz'])
+            ax.set_box_aspect(np.ptp(limits, axis = 1))
+            ax._axis3don = False
+            return ax
+        if kwargs.get('title', False):
+            title = kwargs['title']
+        else:
+            title = ''
+        ax = bloch_sphere()
+        for arg in args:
+            label, color = '| ', 'r'
+            if type(arg) == tuple:
+                if len(arg) == 3: color = arg[2]
+                label = '$|' + arg[1] + '\\rangle$'
+                arg = arg[0]
+            ax.quiver(0, 0, 0, arg.x, arg.y, arg.z, color=color)
+            ax.text(*repel_from_center(arg.x, arg.y, arg.z), label, 'x', \
+                horizontalalignment='center', fontweight='bold', fontsize=11, \
+                color=color)
+        plt.title(title)
+        plt.show()
+    
         
 if __name__ == "__main__":
     q = Qubit()
